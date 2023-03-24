f961075fa8adc2b2655b987dc913f2558d08abbd
Brian Morse <brian.p.morse@gmail.com>
2023-03-23 18:20:01 -0700

    completed basic readme functionality

---
+++ Develop/README.md
@@ -0,0 +1,36 @@
+
+    # bilbo
+        
+    ## Description
+    baggins
+
+    ## Table of Contents
+    - [Installation](#installation)
+
+    - [Usage](#usage)
+
+    - [Contributing](#contribution)
+
+    - [Tests](#tests)
+
+    - [License](#license)
+
+    
+    ## Installation
+    was 
+
+    ## Usage
+    a 
+
+    ## Testing this app
+    american
+
+    ## Contributing
+    great
+
+    ## License
+    This project uses the Apache license. 
+
+    ## Questions
+    For any quesitions regarding this project, please see my Github at frodo or email me at sam. 
+    
--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,10 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer')
-// TODO: Create an array of questions for user input
-const questions = 
+const inquirer = require('inquirer');
+const fs = require("fs");
 
-inquirer
-    .prompt([
+// TODO: Create an array of questions for user input
+// constant containing the questions
+const questions = [
     {
     type: 'input', 
     name: 'title',   
@@ -25,19 +25,19 @@ inquirer
     
     {
         type: 'input', 
-        name: 'usage information',   
+        name: 'usage',   
         message: 'What is the intended way for your project to be used?'
     },
 
     {
         type: 'input', 
-        name: 'contribution guidelines',   
+        name: 'contribution',   
         message: 'What are the guidelines for contributing to this project?'
     },
 
     {
         type: 'input', 
-        name: 'test instructions',   
+        name: 'test',   
         message: 'What are the test instructions for this project? Consider including information about dependencies, specific environment instructions, expected outcomes of project use, and troubleshooting tips.'
     },
 
@@ -47,14 +47,71 @@ inquirer
         message: 'Which license are you using for your project?',
         choices: ['Apache', 'MIT', 'BSD', 'GNU', 'Unlicensed']
     },  
-]);
+
+    {
+        type: 'input', 
+        name: 'github',   
+        message: 'Please provide a link to your public GitHub account:'
+    },
+    {
+        type: 'input', 
+        name: 'email',   
+        message: 'Please provide an email address to list in the readme:'
+    }
+]
 
 // TODO: Create a function to write README file
 
-function writeToFile(fileName, data) {}
+const generateReadme = data => {
+    const {title, description, installation, usage, contribution, test, license, github, email} = data;
+    
+    const readme = `
+    # ${title}
+        
+    ## Description
+    ${description}
+
+    ## Table of Contents
+    - [Installation](#installation)
+
+    - [Usage](#usage)
+
+    - [Contributing](#contribution)
+
+    - [Tests](#tests)
+
+    - [License](#license)
+
+    
+    ## Installation
+    ${installation}
+
+    ## Usage
+    ${usage}
+
+    ## Testing this app
+    ${test}
+
+    ## Contributing
+    ${contribution}
+
+    ## License
+    This project uses the ${license} license. 
+
+    ## Questions
+    For any quesitions regarding this project, please see my Github at ${github} or email me at ${email}. 
+    `;
+    fs.writeFile('README.md', readme, (err) =>
+        err ? console.error(err) : console.log('README.md successfully created!')
+        );
+};
+
+
 
 // TODO: Create a function to initialize app
-function init() {}
+const initApp = () => {
+    inquirer.prompt(questions).then(generateReadme);
+};
 
 // Function call to initialize app
-init();
+initApp();
